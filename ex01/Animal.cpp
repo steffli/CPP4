a5375b5e31773d70bafc532f932a230aefcab6fa
Stephan <stephan@openclaw.fritz.box>
2026-08-04 16:10:15 +0200

    removed =0 in make sound

--- ex01/Animal.cpp
+++ ex01/Animal.cpp
@@ -20,6 +20,10 @@ Animal::~Animal(){
 	std::cout << "Animal destructor called." << std::endl;
 }
 
+void Animal::makeSound() const{
+	std::cout << "Animal sound." << std::endl;
+}
+
 std::string Animal::getType() const{
 	return this->type;
 }
--- ex01/Animal.hpp
+++ ex01/Animal.hpp
@@ -11,7 +11,7 @@ class Animal{
 		Animal(const Animal& other);
 		Animal& operator=(const Animal& other);
 		virtual ~Animal();
-		virtual void makeSound() const = 0;
+		virtual void makeSound() const;
 		std::string getType() const;
 };
 
